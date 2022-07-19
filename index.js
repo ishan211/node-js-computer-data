5b9e9ba8ff2d4c884c43ff30cbf1fe0480f5445f
ishan211 <34925387+ishan211@users.noreply.github.com>
2022-07-19 12:39:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -86,17 +86,3 @@ app.listen(app.get('PORT'), function() {
   console.log(`Application started with port ${PORT}`)
   console.log(`Any Info that appears here means someone (can be you too) clicked onto your IP Grabber.`)
 });
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
-
-keepAlive();
-const monitor = new Monitor({
-    website: '',
-    title: 'NAME',
-    interval: 2
-});
-
-monitor.on('up', (res) => console.log(`${res.website} its on.`));
-monitor.on('down', (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} has stopped.`) );
-monitor.on('error', (error) => console.log(error));
